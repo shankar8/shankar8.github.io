2a0dc38338c865f3fd90b04de0f14168d0845438
shankar8 <shankar.krishna1989@gmail.com>
2016-06-22 01:08:27 -0700

    updateed functions.js

--- js/functions.js
+++ js/functions.js
@@ -11,9 +11,11 @@ $(document).ready(function(){
 
         if(wScroll>introTop-$(this).height()/1.2){
             $(".introTextMain").addClass("isShowing");
+            $(".introImage").addClass("isShowing");
             //console.log("haha");
         }else{
             $(".introTextMain").removeClass("isShowing");
+            $(".introImage").removeClass("isShowing");
         }
 
 
@@ -25,41 +27,41 @@ $(document).ready(function(){
 
 // feature section animation on hover
 $(".featureOne").hover(function() {
-      console.log("hahah");
+      //console.log("hahah");
       $(".featureTextMain").removeClass("showText");
       $(".featureTextOne").addClass("showText");
     }, function() {
-      console.log("getout");
+      //console.log("getout");
       $(".featureTextMain").addClass("showText");
       $(".featureTextOne").removeClass("showText");
     });
 
 $(".featureTwo").hover(function() {
-      console.log("hahah");
+      //console.log("hahah");
       $(".featureTextMain").removeClass("showText");
       $(".featureTextTwo").addClass("showText");
     }, function() {
-      console.log("getout");
+      //console.log("getout");
       $(".featureTextMain").addClass("showText");
       $(".featureTextTwo").removeClass("showText");
     });
 
 $(".featureThree").hover(function() {
-      console.log("hahah");
+      //console.log("hahah");
       $(".featureTextMain").removeClass("showText");
       $(".featureTextThree").addClass("showText");
     }, function() {
-      console.log("getout");
+      //console.log("getout");
       $(".featureTextMain").addClass("showText");
       $(".featureTextThree").removeClass("showText");
     });
 
 $(".featureFour").hover(function() {
-      console.log("hahah");
+      //console.log("hahah");
       $(".featureTextMain").removeClass("showText");
       $(".featureTextFour").addClass("showText");
     }, function() {
-      console.log("getout");
+      //console.log("getout");
       $(".featureTextMain").addClass("showText");
       $(".featureTextFour").removeClass("showText");
     });
@@ -69,7 +71,7 @@ $(".featureFour").hover(function() {
 
 
 
-//Tile scale on hover
+//Tile scale on click
 $(".featureOne").click(function(){
 	$(".featureOneColor").toggleClass("scale");
 });
@@ -119,7 +121,7 @@ $(".featureFour").click(function(){
 	  wContact = $(".contactSection").offset().top,
 	  wFooter = $(".footerSection").offset().top;
 
-		if(wScroll> wContact - $(window).height()){
+        if(wScroll> wContact - $(window).height()){
 	  		$(".iconSetOne, .iconSetTwo").addClass("moveIt");
 	  }else{
 	  		$(".iconSetOne, .iconSetTwo").removeClass("moveIt");
@@ -146,13 +148,13 @@ $(".featureFour").click(function(){
 //---------------------------------------------------------------------------------------------
 // Nav slider fix
 //---------------------------------------------------------------------------------------------
-
+/*
 $("#navButton").click(function(){
       $(".pageContent").toggleClass("pageContentMove");
       console.log("hahaha");
 });
 
-
+*/
 //---------------------------------------------------------------------------------------------
 // contact Modal
 //---------------------------------------------------------------------------------------------
@@ -187,6 +189,43 @@ $(".closeNav").click(function(){
       $("#nav").removeClass("showNav");
 });
 
+
+
+
+
+
+
+//---------------------------------------------------------------------------------------------
+// Dealer Modal
+//---------------------------------------------------------------------------------------------
+
+$(".dealerButton").click(function(){
+      $(".overlayDealer").addClass("showOverlayDealer");
+      $(".dealerModal").addClass("showDealer");
+});
+
+
+$(".closeDealer").click(function(){
+      $(".overlayDealer").removeClass("showOverlayDealer");
+      $(".dealerModal").removeClass("showDealer");
+});
+
+
+
+//---------------------------------------------------------------------------------------------
+// News Modal
+//---------------------------------------------------------------------------------------------
+
+$(".newsButton").click(function(){
+      $(".overlayNews").addClass("showOverlayNews");
+      $(".newsModal").addClass("showNews");
+});
+
+
+$(".closeNews").click(function(){
+      $(".overlayNews").removeClass("showOverlayNews");
+      $(".newsModal").removeClass("showNews");
+});
 
 
 
